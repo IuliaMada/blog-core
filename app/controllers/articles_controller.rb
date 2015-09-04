177537229d4e9e786019dfc2f4a6f1b2eb65a5bb
Mada Iulia <iulia.mada@fortech.ro>
2015-09-04 17:33:36 +0300

    serialize article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,15 +4,13 @@ class ArticlesController < ApplicationController
   # GET /articles
   # GET /articles.json
   def index
-    @articles = Article.all
-
-    render json: @articles
+    articles = Article.all
+    render json: articles
   end
 
   # GET /articles/1
   # GET /articles/1.json
   def show
-    @article = Article.find(params[:id])
     render json: @article
   end
 
